05b5947831d90449a47a4076775e3063d6f6ae58
SecreShall <164751004+SecreShall@users.noreply.github.com>
2024-07-03 18:10:50 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract MyToken is ERC20 {
+  address public contractOwner;
+
+  constructor() ERC20("MyCustomToken", "CustomToken") {
+    contractOwner = msg.sender;
+  }
+
+  modifier exclusiveOwner() {
+    require(msg.sender == contractOwner, "Caller is not the owner");
+    _;
+  }
+
+  // Only owner can mint tokens to specified address
+  function ownerMintToken(address recipient, uint256 qtyToken) public exclusiveOwner {
+    _mint(recipient, qtyToken);
+  }
+
+  // Transfer with event for tracking (optional)
+  function userTransferToken(address recipient, uint256 qtyToken) public returns (bool) {
+    _transfer(msg.sender, recipient, qtyToken);
+    return true;
+  }
+
+  // Burn with event for tracking (optional)
+  function userBurnToken(uint256 qtyToken) public {
+    _burn(msg.sender, qtyToken);
+  }
+}
